7093dbdbc0a101c16e669f7ade474e91fa54d0b6
woong <dndvud123@naver.com>
2023-04-26 22:34:42 +0900

    aws 주소 추가

--- distridute_Linux/settings.py
+++ distridute_Linux/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = djnago_pass()
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".ap-northeast-2.compute.amazonaws.com"
+]
 
 
 # Application definition
