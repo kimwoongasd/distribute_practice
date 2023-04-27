ead49118afdf83154ee17566fa50d759e437b0cc
woong <dndvud123@naver.com>
2023-04-27 22:31:56 +0900

    STATIC_ROOT 추가

--- distridute_Linux/settings.py
+++ distridute_Linux/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 from .passowrd import *
 
@@ -125,3 +126,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
